c750218828fd40a8dbcc4f15b028110a25702405
Ruchin Munjal <emailruchin@gmail.com>
2021-10-30 14:52:15 +0100

    Initial Commit

---
+++ App.Web/HelloWorldModule.cs
@@ -0,0 +1,24 @@
+﻿using Carter;
+using Carter.ModelBinding;
+using Microsoft.AspNetCore.Http;
+
+namespace App.Web
+{
+    public class HelloWorldModule : CarterModule
+    {
+        public HelloWorldModule()
+        {
+            Get("/hello", async (req, res) =>
+            {
+                await res.WriteAsync("Hello World");
+            });
+            Post("/add", async (req, res) =>
+            {
+                var person = await req.Bind<Person>();
+                await res.WriteAsJsonAsync(person);
+            });
+        }
+    }
+
+    public record Person(string FirstName, string LastName);
+}
---
+++ App.Web/Program.cs
@@ -0,0 +1,17 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+var webHost = new WebHostBuilder()
+    .UseKestrel()
+    .ConfigureServices(services =>
+    {
+        services.AddCarter();
+    })
+    .Configure(app =>
+    {
+        app.UseRouting();
+        app.UseEndpoints(builder => builder.MapCarter());
+    }).Build();
+
+webHost.Run();
